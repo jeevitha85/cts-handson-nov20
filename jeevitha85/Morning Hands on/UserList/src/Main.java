5957f5ac6087cecd61c576e7c7a0d8f1ee70bbc2
jeevitha85 <36189352+jeevitha85@users.noreply.github.com>
2020-12-01 07:43:06 +0530

    Update Main.java

--- jeevitha85/Morning Hands on/UserList/src/Main.java
+++ jeevitha85/Morning Hands on/UserList/src/Main.java
@@ -5,11 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		User u1=new User(1,"Ayushi","ayu354");
-		User u2=new User(2,"Riya","riya567");
+		User u1=new User(1,"jeevs","jeevs99");
+		User u2=new User(2,"bubbly","bubbly98");
 		User u3=new User(5,"Trupti","trup876");
-		User u4=new User(4,"Deepika","deep092");
-		User u5=new User(10,"Somu","somu760");
+		User u4=new User(4,"Deepu","deep092");
+		User u5=new User(10,"guru","guru760");
 		ArrayList<User> arr1=new ArrayList<User>();
 		arr1.add(u1);
 		arr1.add(u2);
